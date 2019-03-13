7e616c1c2d92d1564f07a9fab000cbe86b585611
Niklas <niklasolsson94@gmail.com>
2019-03-13 13:28:00 +0000

    Models added and controller for Samples, dont think this is thr right way to continue

---
+++ MusicSampler/Models/Sample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicSampler.Models
+{
+    public class Sample
+    {
+        /// <summary>
+        /// Sample ID
+        /// </summary>
+        [Key]
+        public string SampleID { get; set; }
+
+        /// <summary>
+        /// Title of the sample
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Name of the sample artist
+        /// </summary>
+        public string Artist { get; set; }
+
+        /// <summary>
+        /// URL of the sample blob
+        /// </summary>
+        public string SampleBlobURL { get; set; }
+    }
+}
---
+++ MusicSampler/Models/SampleEntity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicSampler.Models
+{
+    public class SampleEntity : TableEntity
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string SampleBlobURL { get; set; }
+
+        public SampleEntity(string partitionKey, string sampleID)
+        {
+            PartitionKey = partitionKey;
+            RowKey = sampleID;
+        }
+
+        public SampleEntity() { }
+    }
+}
